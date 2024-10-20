22f4f8f51bc254ad4a554f002e48c234dbc4851e
SofiaLemos22 <sofialemos091@gmail.com>
2024-10-20 14:14:06 -0300

    Se realizó el ejercicio 'Registro de Ventas Diarias'

--- Bucles/src/RegistroVentasDiarias.java
+++ Bucles/src/RegistroVentasDiarias.java
@@ -7,9 +7,25 @@ Instrucciones:
  */
 
 
+
+import javax.swing.*;
+
 public class RegistroVentasDiarias {
     public static void main(String[] args) {
+        // Arreglo que contiene los días de la semana
+        String[] semana = {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"};
 
+        // Variable para almacenar el total de ventas
+        int totalVentas = 0;
 
+        // Bucle para registrar las ventas diarias durante 7 días
+        for (int i = 0; i < 7; i++) {
+            // Pedir la cantidad de ventas del día correspondiente
+            int ventas = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la cantidad de ventas para " + semana[i] + ":", "Ventas", JOptionPane.QUESTION_MESSAGE));
+            totalVentas += ventas; // Sumar las ventas al total
+        }
+
+        // Mostrar el total de ventas de la semana
+        JOptionPane.showMessageDialog(null, "El total de ventas de la semana es de: " + totalVentas, "Total Ventas", JOptionPane.INFORMATION_MESSAGE);
     }
 }
